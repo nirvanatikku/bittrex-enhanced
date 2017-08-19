18846fcf1b37306bcf5e6f935ecb414cede1c691
Nirvana Tikku <nirvana@tikku.com>
2017-08-19 16:59:02 -0400

    Refactor background.js (removed browser action)

--- background.js
+++ background.js
@@ -1,20 +1,16 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-chrome.browserAction.setBadgeBackgroundColor({'color':'#0072ed'});
-chrome.browserAction.setBadgeText({text: '×'});
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'Enhancer.go(window, document);'
-  });
-});
+const INITED = '✓';
+const NOTINITED = '×';
+
+chrome.browserAction.setBadgeBackgroundColor({'color':'#0072ed'});
+chrome.browserAction.setBadgeText({text: NOTINITED});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.processing){
-      chrome.browserAction.setBadgeText({text:'✓'});
-    } else {
-      chrome.browserAction.setBadgeText({text:'×'});
+      chrome.browserAction.setBadgeText({text:INITED});
     }
   });
