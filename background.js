e703cc501f4ecc7cbf61bd0266c9813141a6b050
Nirvana Tikku <nirvana@tikku.com>
2017-08-20 13:15:40 -0400

    Update background.js to init settings (storage) and used to init Enhancer

--- background.js
+++ background.js
@@ -9,8 +9,30 @@ chrome.browserAction.setBadgeBackgroundColor({'color':'#0072ed'});
 chrome.browserAction.setBadgeText({text: NOTINITED});
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.processing){
-      chrome.browserAction.setBadgeText({text:INITED});
+    function(request, sender, sendResponse) {
+        if (request.processing){
+            chrome.browserAction.setBadgeText({text:INITED});
+            const settings = {};
+            chrome.storage.sync.get('bittrex-enhanced-tvChart', function(data) {
+                settings.tvChart = data['bittrex-enhanced-tvChart'];
+                chrome.storage.sync.get('bittrex-enhanced-usdVal', function(data) {
+                    settings.usdVal = data['bittrex-enhanced-usdVal'];
+                    sendResponse(settings);
+                });
+            });
+        }
+        return true;
     }
-  });
+);
+
+// Settings
+chrome.storage.sync.get('bittrex-enhanced-usdVal', function(data) {
+    if(!data['bittrex-enhanced-usdVal']){
+        chrome.storage.sync.set({'bittrex-enhanced-usdVal': true});
+    }
+});
+chrome.storage.sync.get('bittrex-enhanced-tvChart', function(data) {
+    if(!data['bittrex-enhanced-tvChart']){
+        chrome.storage.sync.set({'bittrex-enhanced-tvChart': true});
+    }
+});
