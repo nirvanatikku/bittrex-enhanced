96ba1efc3f759b961386a331c7b55d4101a63ad3
Nirvana Tikku <nirvana@tikku.com>
2017-08-24 21:15:40 -0400

    Offer TradingView chart settings (defaults + override within popup) (Fixes #1)
    - Refactored some `var` statements => `let`
    - Refactored various constants
    - Streamline chrome.sync get using single call
    - Updated settings to include sections + TradingView settings

--- background.js
+++ background.js
@@ -4,6 +4,24 @@
 
 const INITED = '✓';
 const NOTINITED = '×';
+const TRADING_VIEW_DEFAULT_OPTIONS = {
+    width: '100%',
+    height: 400,
+    interval: 30,
+    timezone: 'America/New_York',
+    theme: 'Light',
+    style: 1,
+    locale: 'en',
+    enable_publishing: false,
+    withdateranges: true,
+    studies: [],
+    hide_side_toolbar: false,
+    show_popup_button: true,
+    popup_width: 1000,
+    popup_height: 650,
+    allow_symbol_change: true,
+    hideideas: true
+};
 
 chrome.browserAction.setBadgeBackgroundColor({'color':'#0072ed'});
 chrome.browserAction.setBadgeText({text: NOTINITED});
@@ -13,12 +31,16 @@ chrome.runtime.onMessage.addListener(
         if (request.processing){
             chrome.browserAction.setBadgeText({text:INITED});
             const settings = {};
-            chrome.storage.sync.get('bittrex-enhanced-tvChart', function(data) {
+            chrome.storage.sync.get([
+                'bittrex-enhanced-tvChart', 
+                'bittrex-enhanced-usdVal', 
+                'bittrex-enhanced-tvChartOpts'
+            ], function(data) {
+                console.log(data);
                 settings.tvChart = data['bittrex-enhanced-tvChart'];
-                chrome.storage.sync.get('bittrex-enhanced-usdVal', function(data) {
-                    settings.usdVal = data['bittrex-enhanced-usdVal'];
-                    sendResponse(settings);
-                });
+                settings.usdVal = data['bittrex-enhanced-usdVal'];
+                settings.tradingViewOpts = data['bittrex-enhanced-tvChartOpts'] || {};
+                sendResponse(settings);
             });
         }
         return true;
@@ -26,13 +48,18 @@ chrome.runtime.onMessage.addListener(
 );
 
 // Settings
-chrome.storage.sync.get('bittrex-enhanced-usdVal', function(data) {
+chrome.storage.sync.get([
+    'bittrex-enhanced-usdVal',
+    'bittrex-enhanced-tvChart',
+    'bittrex-enhanced-tvChartOpts'
+], function(data) {
     if(!data['bittrex-enhanced-usdVal']){
         chrome.storage.sync.set({'bittrex-enhanced-usdVal': true});
     }
-});
-chrome.storage.sync.get('bittrex-enhanced-tvChart', function(data) {
     if(!data['bittrex-enhanced-tvChart']){
         chrome.storage.sync.set({'bittrex-enhanced-tvChart': true});
+    }
+    if(!data['bittrex-enhanced-tvChartOpts']){
+        chrome.storage.sync.set({'bittrex-enhanced-tvChartOpts': TRADING_VIEW_DEFAULT_OPTIONS})
     }
 });
--- settings.js
+++ settings.js
@@ -1,12 +1,27 @@
 function getConfigNodes(){ // buttons
     let configShowUsdVal = document.getElementById('configShowUsdVal');
     let configShowTradingViewChart = document.getElementById('configShowTradingViewChart');
+    let configTradingViewOptsElmIds = [
+        'tvConfWidth',
+        'tvConfHeight',
+        'tvConfInterval',
+        'tvConfTimezone',
+        'tvConfTheme',
+        'tvConfLocale',
+        'tvConfStudies',
+        'tvConfStyle'
+    ];
+    let configTradingViewOpts = {};
+    configTradingViewOptsElmIds.forEach(function(elmId){
+        configTradingViewOpts[elmId] = document.getElementById(elmId);
+    });
     return {
         usdVal: configShowUsdVal,
-        tvChart: configShowTradingViewChart
+        tvChart: configShowTradingViewChart,
+        tvChartOpts: configTradingViewOpts
     }
 }
-function updateConfig(prop){
+function updateConfigBool(prop){
     return function(evt){
         let data = {};
         let tgt = evt.target;
@@ -19,22 +34,84 @@ function updateConfig(prop){
         chrome.storage.sync.set(data);
     };
 }
-function initConfig(){
-    let configNodes = getConfigNodes();
-    chrome.storage.sync.get('bittrex-enhanced-usdVal', function(data) {
+function updateConfigTvObj(prop){
+    return function(evt){
+        let data = {};
+        let tgt = evt.target;
+        let isSelect = tgt.nodeName === 'SELECT';
+        let val = isSelect ? Array.prototype.slice.call(tgt.options).map(function(opt){
+                                                  if(opt.selected) return opt.value;
+                                              }) : tgt.value;
+        if(tgt.nodeName === 'SELECT'){
+            val = val.filter(function(n){ return n != undefined })
+            if(tgt.getAttribute('multiple') !== 'multiple'){
+                if(val.length > 0){
+                    val = val[0];
+                }
+            }
+        }
+        chrome.storage.sync.get('bittrex-enhanced-tvChartOpts', function(data){
+            let opts = data['bittrex-enhanced-tvChartOpts']; 
+            opts[prop] = val;
+            chrome.storage.sync.set({'bittrex-enhanced-tvChartOpts': opts});
+        });
+    };
+}
+function initConfigDropdown(prop, node, val){
+    // assigns the value in a dropdown and attaches event listener
+    let opts = node.options;
+    Array.prototype.slice.call(opts).forEach(function(opt){
+        if(opt.value === String(val)){
+            opt.selected = true;
+        }
+    });
+    node.onchange = updateConfigTvObj(prop);
+}
+function initConfigMultiselect(prop, node, values){
+    // assigns the values in a multiselect and attaches event listener 
+    // (e.g. [{option: 'x', selected: true|false}, ..])
+    let opts = node.options;
+    Array.prototype.slice.call(opts).forEach(function(opt){
+        if(values.indexOf(opt.value) > -1){
+            opt.selected = true;
+        }
+    });
+    node.onchange = updateConfigTvObj(prop);
+}
+function initConfigText(prop, node, val){
+    node.value = val;
+    node.onblur = updateConfigTvObj(prop);
+}
+function initConfig(configNodes){
+    chrome.storage.sync.get([
+        'bittrex-enhanced-usdVal',
+        'bittrex-enhanced-tvChart'
+    ], function(data) {
         configNodes.usdVal.setAttribute('data-enabled', 
             data ? Boolean(data['bittrex-enhanced-usdVal']) : true);
-    });
-    chrome.storage.sync.get('bittrex-enhanced-tvChart', function(data) {
         configNodes.tvChart.setAttribute('data-enabled', 
             data ? Boolean(data['bittrex-enhanced-tvChart']) : true);
     });
 }
+function initConfigTradingViewOpts(nodes){
+    chrome.storage.sync.get('bittrex-enhanced-tvChartOpts', function(data){
+        data = data['bittrex-enhanced-tvChartOpts'];
+        initConfigText('width', nodes.tvConfWidth, data.width);
+        initConfigText('height', nodes.tvConfHeight, data.height);
+        initConfigDropdown('interval', nodes.tvConfInterval, data.interval);
+        initConfigDropdown('timezone', nodes.tvConfTimezone, data.timezone);
+        initConfigDropdown('theme', nodes.tvConfTheme, data.theme);
+        initConfigDropdown('style', nodes.tvConfStyle, data.style);
+        initConfigDropdown('locale', nodes.tvConfLocale, data.locale);
+        initConfigMultiselect('studies', nodes.tvConfStudies, data.studies);
+    });
+}
 function initForm(){
     let configNodes = getConfigNodes();
-    configNodes.usdVal.onclick = updateConfig('usdVal');
-    configNodes.tvChart.onclick = updateConfig('tvChart');
+    initConfig(configNodes);
+    initConfigTradingViewOpts(configNodes.tvChartOpts);
+    configNodes.usdVal.onclick = updateConfigBool('usdVal');
+    configNodes.tvChart.onclick = updateConfigBool('tvChart');
 }
 
-initConfig();
 initForm();
